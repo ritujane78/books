b36ec89f2ea62198b3d564242fb05d8cf7d4440a
Ritu Bafna <ritujane78@gmail.com>
2025-08-23 13:42:42 +0545

    path variable

--- src/main/java/com/jane/books/controller/BooksController.java
+++ src/main/java/com/jane/books/controller/BooksController.java
@@ -2,6 +2,7 @@ package com.jane.books.controller;
 
 import com.jane.books.entity.Book;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -31,5 +32,19 @@ public class BooksController {
     @GetMapping("/api/books")
     public List<Book> getBooks(){
         return books;
+    }
+
+    @GetMapping("/api/books/{title}")
+    public Book getBookByTitle(@PathVariable String title){
+//        for(Book book: books){
+//            if(book.getTitle().equalsIgnoreCase(title)){
+//                return book;
+//            }
+//        }
+//        return null;
+        return (books.stream()
+                .filter(book -> book.getTitle().equalsIgnoreCase(title))
+                .findFirst()
+                .orElse(null));
     }
 }
